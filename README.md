a0931f0e7fba3a9ad4317aa7680e979346d9bf89
ynot01 <ynot000001@gmail.com>
2025-08-05 15:29:11 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# No Fonts
+
+This unsets all fonts in the game, forcing it to fallback to a much more pleasing default font.
+
+![](./dumpster_preview.webp)
+
+![](./prole_menu.webp)
+
+## Installing
+
+This project was built using BepInEx Bleeding Edge Artifact #738
+
+It can be downloaded here: https://builds.bepinex.dev/projects/bepinex_be, ensure you download the one labeled with "IL2CPP" as ANEURISM IV is built with IL2CPP
+
+Drop the BepInEx into your game folder, then download the mod from Releases on the right, then navigate to BepInEx/plugins and drop the mod in there
+
+## Building
+
+Place this project folder under https://github.com/FatigueDev/aneurism_iv_modding/ and follow `dotnet build` instructions
---
+++ no_fonts.cs
@@ -0,0 +1,36 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+using TMPro;
+using UnityEngine;
+
+namespace no_fonts
+{
+
+	public class NoFonts_Component : MonoBehaviour 
+	{
+		void Update()
+		{
+            var texts = GameObject.FindObjectsByType<TMP_Text>(UnityEngine.FindObjectsSortMode.None);
+            foreach (TMP_Text txt in texts)
+            {
+                txt.font = null;
+            }
+        }
+	}
+
+	[BepInPlugin("no_fonts", "No Fonts", "0.0.1")]
+	public class NoFonts : BasePlugin
+	{
+		internal static new ManualLogSource Log;
+
+		public override void Load()
+		{
+			Log = base.Log;
+
+			AddComponent<NoFonts_Component>();
+
+			Log.LogInfo($"Plugin no_fonts loaded successfully.");
+		}
+	}
+}
